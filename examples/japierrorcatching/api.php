eb4261de93e062bf31001cfce774977ebe4b816e
gordonmcvey <grj.mcvey@gmail.com>
2025-05-27 18:09:30 +0100

    Error Handling: Added error handling examples

---
+++ examples/japierrorcatching/api.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Copyright © 2015 Docnet, 2025 Gordon McVey
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Docnet\JAPI\examples\japierrorcatching;
+
+use Docnet\JAPI\error\JsonErrorHandler;
+use Docnet\JAPI\JAPI;
+use Docnet\JAPI\middleware\CallStackFactory;
+use gordonmcvey\httpsupport\enum\factory\StatusCodeFactory;
+use gordonmcvey\httpsupport\request\Request;
+use gordonmcvey\httpsupport\request\RequestInterface;
+
+/**
+ * Example of error handling when a problem occurs inside JAPI's dispatch cycle.  Note that standard handling can only
+ * deal with \Throwable errors.  Things outside this (like warnings, deprecation notices, etc) require additional logic
+ * to handle and are dealt with in the japierroruncaught example.
+ */
+
+// Includes or Auto-loader
+define('BASE_PATH', dirname(__DIR__, 2));
+
+require_once BASE_PATH . '/vendor/autoload.php';
+
+// Demo
+(new JAPI(new CallStackFactory(), new JsonErrorHandler(new StatusCodeFactory(), exposeDetails: true)))
+    ->bootstrap(
+        function (RequestInterface $request) {
+            return new \stdClass();
+        },
+        Request::fromSuperGlobals()
+    )
+;
